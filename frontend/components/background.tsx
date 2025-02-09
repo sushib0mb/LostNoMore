560bc6cd36fe8fb8a43735ece06b9e209f032679
sushib0mb <azuchitaizo@gmail.com>
2025-02-08 23:48:35 -0500

    Added api call to get adventure data from chapgpt

--- frontend/components/background.tsx
+++ frontend/components/background.tsx
@@ -2,15 +2,27 @@ import React from "react";
 import ExplorationPrompt from "./exploration";
 import AdventureInput from "./adventureinput";
 
-function TravelExplorer() {
+function TravelExplorer({setInput, handleSubmit, chatArray}) {
   return (
     <div className="flex overflow-hidden flex-col justify-center items-start px-12 py-16 bg-blue-900 border border-black border-solid shadow-[0px_4px_4px_rgb(26,46,7E)] max-md:px-5">
       <div className="flex flex-col px-6 py-8 max-w-full bg-neutral-700 rounded-[50px] w-[610px] max-md:pl-5">
         <ExplorationPrompt />
-        <AdventureInput />
+        <Chats chats={chatArray}/>
+        <AdventureInput setInput={setInput} handleSubmit={handleSubmit}/>
       </div>
     </div>
   );
 }
 
 export default TravelExplorer;
+
+const Chats = ({ chats }: { chats: string[] }) => {
+  return (
+    console.log(chats),
+    <div>
+      {chats.map((chat, index) => (
+        <p key={index}>{chat}</p>
+      ))}
+    </div>
+  );
+}
--- frontend/pages/chat.tsx
+++ frontend/pages/chat.tsx
@@ -1,10 +1,16 @@
-import AdventureInput from "@/components/adventureinput";
 import TravelExplorer from "@/components/background";
 import React from "react";
 import { useState } from "react";
 
+export default function Chat() {
+    const [input, setInput] = useState("")
+    // state thats an array 
+    const [chats, setChats] = useState([])
+    const [isBlocked, setIsBlocked]=useState(false)
+    console.log(input)
 
-const handleSubmit = async (userResponse: string) => {
+    
+const fetchResponse  = async (userResponse) => {
     try {
         const response = await fetch("http://localhost:8000/ai", {
             method: "POST",
@@ -22,29 +28,24 @@ const handleSubmit = async (userResponse: string) => {
 
         const responseData = await response.json();
         console.log(responseData)
+        //set state 
+        setChats((chats) => {
+            return [...chats, responseData];
+        })
+        // setIsBlocked((isBlocked)) => true;
     } catch (error) {
         console.error("Error:", error);
     }
 };
 
-
-function Chat() {
-    const [input, setInput] = useState("")
-
+    const handleSubmit = () => {
+        // setIsBlocked((isBlocked)) => true;
+        fetchResponse(input)
+    }
+    
   return (
-    <>
-            <>
-        <p>chat</p>
-        <input onChange={(e)=> {
-            setInput(e.target.value)
-        }
-        }></input>
-        <button onClick={() =>{
-            handleSubmit(input)
-        }}>hello</button>
-        </>
-   
-        <TravelExplorer />
+    <>   
+        <TravelExplorer setInput={setInput} handleSubmit={handleSubmit} chatArray={chats}/>
         </>
   );
 }
